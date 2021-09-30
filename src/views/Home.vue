973112f3a674c721b136cfd89b65ec4d767645dd
ManuelDizen <mdizenhaus@itba.edu.ar>
2021-09-30 17:16:19 -0300

    Cambios varios. NavBar reversionada, home rediseñado, borro basura, agrego sources de fondos y logos. No updateo package-lock.json (creo que es algo que debe poner npm ahi)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="page">
+  <!--<div id="page">
     <v-container class="background2" fluid>
       <v-row class="text-center">
         <v-col class="mb-4">
@@ -23,6 +23,39 @@
         </v-row>
       </v-container>
     </v-container>
+  </div>-->
+  <div id="home">
+    <div id="welcome">
+      <span id="welcomeText">¡Bienvenido, {{ username }}!</span>
+    </div>
+    <div id="mainBody">
+      <v-container class="container--fluid">
+        <v-row>
+          <v-col>
+            <v-card class="md6 card">
+              <v-icon>mdi-heart-multiple</v-icon>
+            </v-card>
+          </v-col>
+          <v-col>
+            <v-card class="md6 card">
+              <v-icon>mdi-magnify</v-icon>
+            </v-card>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-card class="md6 card">
+              <v-icon>mdi-magnify</v-icon>
+            </v-card>
+          </v-col>
+          <v-col>
+            <v-card class="md6 card">
+              <v-icon>mdi-help-circle-outline</v-icon>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </div>
   </div>
 </template>
 
@@ -42,17 +75,41 @@
 </script>
 
 <style scoped>
-#page{
-  background-color:aqua;
-  height: 100vh;
-  width: 100vw;
-  background-position:center;
-  background-size:cover;
-}
+
+  #page{
+    background-color:aqua;
+    height: 100vh;
+    width: 100vw;
+    background-position:center;
+    background-size:cover;
+  }
+  #home{
+    background-image: url("../assets/fondoHome.jpg");
+    height: 100vh;
+    background-size: cover;
+  }
+  #welcome{
+    background: rgba(255,255,255,0.5);
+    font-size: xxx-large;
+    color: black;
+    height: 20%;
+  }
+  #welcomeText{
+    color:black;
+    size: auto;
+  }
+
+  #mainBody{
+    background:rgba(255,255,255,0.8);
+    height:100%;
+  }
   .card{
     background-color: lightslategrey;
-    width: 80%;
-    height:100%;
-    position:relative;
+    width: 100%;
+    height:200px;
+    position:center;
+    border-color: black;
+    border-width: thick;
+
   }
 </style>
