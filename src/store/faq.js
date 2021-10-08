6ab9ef4f77a7f4cf266afbe6d0429fc7aee08caa
ManuelDizen <mdizenhaus@itba.edu.ar>
2021-10-08 16:30:55 -0300

    Armo todo help

---
+++ src/store/faq.js
@@ -0,0 +1,23 @@
+export default{
+    questions: [
+        {
+            id: 1,
+            q: "¿Que es PushApp?",
+            a: "PushApp es una aplicación, disponible en versión web y móvil, que viene a ayudar a todas las personas" +
+                ", sin importar estado físico, ocupación, ni tiempo disponible, a sumar actividad física a sus vidas" +
+                " a traves de rutinas adaptables y completamente personalziables "
+        },
+        {
+            id:2,
+            q: "¿Es gratuita?",
+            a: "¡Si! PushApp es completamente gratuita. Solo se requiere un registro inicial para validar la " +
+                "información una vez que se busca "
+        },
+        {
+            id:3,
+            q:"¿Es necesario estar en forma para usar la aplicación?",
+            a: "No. La intención es que cualquier persona que bsuque un cambio en su vida pueda encontrar en esta " +
+                "aplicación una manera facil e interactiva de progresar y mejorar su estado físico."
+        }
+    ]
+}
--- src/views/Help.vue
+++ src/views/Help.vue
@@ -1,13 +1,122 @@
 <template>
-  <h1>Help</h1>
+  <div class="help">
+    <div id="title">
+      <b>Help</b>
+    </div>
+    <div class="body justify-center">
+      <v-expansion-panels>
+        <v-expansion-panel
+            v-for="question in questions"
+            :key="question.id" class="panel">
+          <v-expansion-panel-header class="header">
+            {{ question.q }}
+          </v-expansion-panel-header>
+          <v-expansion-panel-content class="content">
+            {{question.a}}
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </div>
+    <div id="title2"><b>Contactenos</b></div>
+    <div class="contacto">
+      <v-form ref="form" v-model="valid" lazy-validation>
+        <v-text-field class="black--text" v-model="nombre" label="Nombre" required outlined></v-text-field>
+        <v-text-field class="black--text" v-model="email" :rules="emailRules" label="E-mail" outlined> </v-text-field>
+        <v-textarea
+            class="black--text"
+            v-model="title"
+            label="Message"
+            outlined
+            maxlength="400"
+            full-width
+            single-line
+            filled
+            shaped
+        ></v-textarea>
+      </v-form>
+    </div>
+    <v-btn
+        class="boton"
+        rounded
+        color="blue"
+        dark
+        @click="enviarFormulario"
+    >
+      Enviar
+    </v-btn>
+
+  </div>
 </template>
 
 <script>
+import store from "@/store/faq";
+
 export default {
-  name: "Help.vue"
-}
+  name: "Help.vue",
+  data(){
+    return{
+      questions: store.questions,
+    };
+  },
+  methods:{
+    enviarFormulario(){
+      /*Falta ver que hace el formulario*/
+    }
+  }
+};
 </script>
 
 <style scoped>
+.help{
+  font-family: "Raleway", sans-serif;
+  color: black;
+  background-image: url("../assets/FondoHelp.jpg");
+  background-size: cover;
+  height:100%;
+  vertical-align: middle;
+
+}
+#title, #title2{
+  font-family: "Raleway", sans-serif;
+  font-size: xxx-large;
+  padding-bottom: 0;
+  margin-top: auto;
+  margin-bottom: 30px;
+  vertical-align: middle;
+}
+#title2{
+  margin-top: 30px;
+}
+.panel{
+  display:block;
+  margin-left: auto;
+  margin-right:auto;
+}
+.header{
+  justify-content: center;
+  font-size: x-large;
+}
+.content{
+  font-size: large;
+}
+.body{
+  width: 80vw;
+  justify-content: center;
+  margin:auto;
+}
+.contacto{
+  width: 80%;
+  justify-content: center;
+  margin-left: auto;
+  margin-right:auto;
+  background-color: white;
+  margin-bottom: 0;
+  padding-top: 15px;
+  padding-left: 10px;
+  padding-right: 10px;
+}
+.boton{
+  margin-top: 15px;
+}
 
 </style>
