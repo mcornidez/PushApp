e2c2dadf2173e561414ed85a4008e8678e4164ec
Milagros Cornidez <mcornidez@itba.edu.ar>
2021-10-12 15:48:14 -0300

    Github hace lo que quiere

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -29,10 +29,6 @@
 <script>
 import {Credentials2} from "../../api/user";
 import {mapActions} from "vuex";
-<<<<<<< HEAD
-=======
-
->>>>>>> 8ea5e7ba6216349281f4172592f284474959691d
 export default {
   name: "Register",
   data(){
