8862c98ffc8b1f6cfb458a6db1eb6cff0ca22af0
ManuelDizen <mdizenhaus@itba.edu.ar>
2021-10-13 13:19:42 -0300

    Funciona para modificar usuario

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,19 +1,18 @@
 <template>
   <div class="profile">
     <!-- Template extraido de https://stackoverflow.com/questions/63729323/vuetify-custom-component-for-user-profile -->
-    <v-card class="mx-auto" width="80%" tile>
-      <v-img height="20vh" src="https://cdn.vuetifyjs.com/images/cards/server-room.jpg"></v-img>
-      <v-list-item color="rgba(0, 0, 0, .4)">
-        <v-list-item-content>
-          <v-list-item-title class="title text--black">{{ $user.username }}</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-    </v-card>
+    <div class="title width:80%;">Datos de usuario: {{ $user.username }}</div>
     <div class="grid-container">
       <div class="form-item">
         <div>
           <div class="item">
-            <div class="align-center mt-6" v-if="!changeName">Nombre: {{ $user.firstName }}</div>
+            <div class="align-center pl-5 mt-6"><b>Nombre de usuario: </b>{{ $user.username }}</div>
+          </div>
+          <div class="item">
+            <div class="align-center pl-5 mt-6"><b>Mail: </b>{{ $user.email }}</div>
+          </div>
+          <div class="item">
+            <div class="align-center pl-5 mt-6" v-if="!changeName"><b>Nombre: </b> {{ $user.firstName }}</div>
             <div class="width:100%; ml-5" v-else>
               <v-text-field class="mt-6" outlined label="Nombre: " v-model="firstname" id="name"></v-text-field>
             </div>
@@ -22,34 +21,16 @@
             </button>
           </div>
           <div class="item">
-            <div class="align-center mt-6" v-if="!changeLast">Apellido: {{ $user.lastName }}</div>
+            <div class="align-center pl-5 mt-6" v-if="!changeLast"><b>Apellido: </b>{{ $user.lastName }}</div>
             <div class="width:100%; ml-5" v-else>
-              <v-text-field outlined label="Apellido: " v-model="lastname" id="surname"></v-text-field>
+              <v-text-field class="mt-6" outlined label="Apellido: " v-model="lastname" id="surname"></v-text-field>
             </div>
             <button @click="modifyLast" class="btn pa-0 mt-6">
               <v-icon left>mdi-pencil-box-outline</v-icon>
             </button>
           </div>
           <div class="item">
-            <div class="align-center mt-6">Nombre de usuario: {{ $user.username }}</div>
-            <!--<div class="width:100%; ml-5" v-else>
-              <v-text-field outlined label="Nombre de usuario: " v-model="username" id="username"></v-text-field>
-            </div> -->
-            <!-- <button @click="modifyUser" class="btn pa-0 mt-6">
-              <v-icon left>mdi-pencil-box-outline</v-icon>
-            </button> -->
-          </div>
-          <div class="item">
-            <div class="align-center mt-6">Mail: {{ $user.email }}</div>
-            <!--<div class="width:100%; ml-5" v-else>
-              <v-text-field outlined label="Mail: " v-model="mail" id="mail"></v-text-field>
-            </div>
-            <button @click="modifyEmail" class="btn pa-0 mt-6">
-              <v-icon left>mdi-pencil-box-outline</v-icon>
-            </button> -->
-          </div>
-          <div class="item">
-            <div class="align-center mt-6" v-if="!changeGender">Género: {{ $user.gender }}</div>
+            <div class="pl-5 mt-6" v-if="!changeGender"><b>Género: </b>{{ $user.gender }}</div>
             <div class="width:100%; ml-5" v-else>
               <v-text-field class="mt-6" outlined label="Género: " v-model="gender" id="gender"></v-text-field>
             </div>
@@ -59,6 +40,11 @@
           </div>
           <button v-if="changeName || changeLast || changeUser || changeEmail || changeGender" @click="saveChanges" id="sendBtn">Guardar cambios</button>
         </div>
+      </div>
+      <div class="grid-item">
+        <div class="title pt-40">Foto de perfil</div>
+        <img src="https://alvareztaxinc.com/wp-content/uploads/2016/05/icon-user-default.png" alt="ProfilePic"
+             class="image">
       </div>
     </div>
   </div>
@@ -72,13 +58,6 @@ export default {
   name: "Profile",
   data(){
     return{
-      /*username: null,
-      firstname: null,
-      lastname: null,
-      password: null,
-      gender:null,
-      picture: null,
-      mail:null,*/
       changeName: false,
       changeLast: false,
       changeUser: false,
@@ -102,7 +81,6 @@ export default {
     },
     canOperate() {
       return this.$isLoggedIn
-      // BORRO this.sport PARA PROBAR
     },
     canAbort() {
       return this.$isLoggedIn && this.controller
@@ -147,18 +125,15 @@ export default {
       try {
         let nombre = this.changeName ? document.getElementById("name").value : this.username;
         let apellido = this.changeLast ? document.getElementById("surname").value : this.lastname;
-        let pass = this.changePass ? document.getElementById("password").value : this.password;
-        let mail = this.changeEmail ? document.getElementById("mail").value : this.mail;
-        let user = this.changeUser ? document.getElementById("username").value : this.username;
         let gender = this.changeGender ? document.getElementById("gender").value : this.gender;
-        const credentials = new Credentials2(user, pass, mail, nombre, apellido, gender);
+        const credentials = new Credentials2(this.username, this.password, this.mail, nombre, apellido, gender);
         await this.$modifyUser(credentials);
-        this.changeGender = this.changeName = this.changeLast = this.changeEmail = this.changeUser = this.changePass = false;
+        this.changeGender = this.changeName = this.changeLast = false;
         const redirectPath = this.$route.query.redirect || "/Profile";
         await this.$router.push(redirectPath);
       }
       catch(e){
-        this.setResult(e);
+        this.setResult(e.description);
       }
     }
   }
@@ -173,6 +148,18 @@ export default {
   background-color: white;
   /*display: flex;*/
 }
+.title{
+  font-size: xx-large;
+  font-family: "Raleway", sans-serif;
+  background-color: white;
+  width:80%;
+  margin: auto;
+  justify-content: center;
+  padding-top: 15px;
+  border-top-left-radius: 25px;
+  border-top-right-radius: 25px;
+  border-bottom: 1px solid black;
+}
 .profile{
   background-image: url("../assets/ProfileFondo.jpg");
   background-size: cover;
@@ -180,14 +167,26 @@ export default {
   background-attachment: fixed;
 }
 
+
+.image{
+  border-radius: 50%;
+  width: 300px;
+  height: 300px;
+  margin-top: 40px;
+  vertical-align: middle;
+  align-items: center;
+}
 .grid-container {
   display: inline-grid;
+  padding-top: 15px;
   background-color: rgba(255,255,255);
   width: 80%;
-  height: 100vh;
+  height: 80vh;
   grid-template-columns: repeat(2, minmax(0, 1fr));
   grid-column-gap: 30px;
   grid-row-gap: 30px;
+  border-bottom-right-radius: 25px;
+  border-bottom-left-radius: 25px;
 }
 
 .item{
@@ -196,6 +195,8 @@ export default {
   display:flex;
   justify-content: space-between;
   align-content: center;
+  border: 1px solid black;
+  border-radius: 15px;
 }
 
 .form-item{
