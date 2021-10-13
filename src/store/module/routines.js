2c565d1d75ad8833274760159ced94ef88a572f1
Milagros Cornidez <67882015+mcornidez@users.noreply.github.com>
2021-10-12 23:27:50 -0300

    Update routines.js

--- src/store/module/routines.js
+++ src/store/module/routines.js
@@ -1,11 +1,9 @@
 import {RoutinesApi} from "../../../api/routines";
 
-
 export default {
     namespaced: true,
     state: {
-        routines: [],
-        currentRoutine: null
+        routines: []
     },
     getters: {
         findIndex(state) {
@@ -18,8 +16,14 @@ export default {
         push(state, routine) {
             state.items.push(routine)
         },
-        updateCurrentRoutine(state, currentRoutine) {
-            state.currentRoutine = currentRoutine
+        replace(state, index, sport) {
+            state.items[index] = sport
+        },
+        splice(state, index) {
+            state.items.splice(index, 1)
+        },
+        replaceAll(state, sports) {
+            state.items = sports
         }
     },
     actions: {
@@ -27,8 +31,34 @@ export default {
             const result = await RoutinesApi.add(routine)
             if (!getters.findIndex(result))
                 commit('push', result)
-            commit('updateCurrentRoutine', result)
             return result
         },
+        async modify({getters, commit}, routine) {
+            const result = await RoutinesApi.modify(routine)
+            const index = getters.findIndex(result)
+            if (index >= 0)
+                commit('replace', index, result)
+            return result
+        },
+        async delete({getters, commit}, routine) {
+            await RoutinesApi.delete(routine.id)
+            const index = getters.findIndex(routine)
+            if (index >= 0)
+                commit('splice', index)
+        },
+        async get({state, getters, commit}, routine) {
+            const index = getters.findIndex(routine)
+            if (index >= 0)
+                return state.items[index]
+
+            const result = await RoutinesApi.get()
+            commit('push', result)
+            return result
+        },
+        async getAll({commit}, controller) {
+            const result = await RoutinesApi.getAll(controller)
+            commit('replaceAll', result)
+            return result
+        }
     },
 }
