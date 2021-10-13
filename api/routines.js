292d4dba1c83f6eb87de4db4b35be3cd5013faeb
Milagros Cornidez <67882015+mcornidez@users.noreply.github.com>
2021-10-12 23:27:12 -0300

    Update routines.js

--- api/routines.js
+++ api/routines.js
@@ -9,6 +9,21 @@ class RoutinesApi {
     static async add(routine, controller) {
         return await Api.post(RoutinesApi.getUrl(), true, routine, controller)
     }
+    static async modify(routine, controller) {
+        return await Api.put(RoutinesApi.getUrl(routine.id), true, routine, controller)
+    }
+
+    static async delete(id, controller) {
+        return await Api.delete(RoutinesApi.getUrl(id), true, controller)
+    }
+
+    static async get(id, controller) {
+        return await Api.get(RoutinesApi.getUrl(id),true,  controller)
+    }
+
+    static async getAll(controller) {
+        return await Api.get(RoutinesApi.getUrl(), true, controller)
+    }
 }
 class Routine {
     constructor(name, detail, isPublic, difficulty) {
